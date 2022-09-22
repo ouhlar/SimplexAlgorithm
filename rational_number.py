aa45122f5c8a824bb278edf1a59d0adaf0ef7f17
duchzjety <oliverkolombo@gmail.com>
2022-09-22 21:47:50 +0200

    reduce rational number

--- rational_number.py
+++ rational_number.py
@@ -1,3 +1,6 @@
+from typing import Tuple
+
+
 class RationalNumber:
     def __init__(self, numerator: int, denominator: int) -> None:
         self._minus: bool = False
@@ -13,8 +16,16 @@ class RationalNumber:
                 self._minus = True
             else:
                 self._minus = False
-            self._numerator: int = abs(numerator)
-            self._denominator: int = abs(denominator)
+            self._numerator, self._denominator = self._reduce(abs(numerator), abs(denominator))
 
     def __str__(self) -> str:
         return (self._minus and '-' or ' ') + str(self._numerator) + "/" + str(self._denominator)
+
+    def _gcd(self, n: int, d: int) -> int:
+        if n == 0:
+            return d
+        return self._gcd(d % n, n)
+
+    def _reduce(self, numerator: int, denominator: int) -> Tuple[int, int]:
+        gcd_num: int = self._gcd(numerator, denominator)
+        return numerator // gcd_num, denominator // gcd_num
