245859615f880d1cdad38fecc43d50a4e21aff3e
duchzjety <oliverkolombo@gmail.com>
2022-09-24 15:30:34 +0200

    reorganize repo and add support for arithmetic operators

--- rational_number.py
+++ rational_number.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import Tuple, Union
 
 
 class RationalNumber:
@@ -18,7 +18,7 @@ class RationalNumber:
             self._numerator, self._denominator = self._reduce(abs(numerator), abs(denominator), minus)
 
     def __str__(self) -> str:
-        return str(self._numerator) + "/" + str(self._denominator)
+        return "{}/{}".format(str(self._numerator), str(self._denominator))
 
     def _gcd(self, n: int, d: int) -> int:
         if n == 0:
@@ -36,22 +36,53 @@ class RationalNumber:
         return self._denominator
 
     def __eq__(self, rn: "RationalNumber") -> bool:
-        if not isinstance(rn, RationalNumber):
-            raise TypeError("Wrong type of comparing number")
         return self.__dict__ == rn.__dict__
 
     def __gt__(self, rn: "RationalNumber") -> bool:
-        if not isinstance(rn, RationalNumber):
-            raise TypeError("Wrong type of comparing number")
         return self._numerator * rn.get_denominator() > \
             rn.get_numerator() * self.get_denominator()
 
     def __ge__(self, rn: "RationalNumber") -> bool:
-        if not isinstance(rn, RationalNumber):
-            raise TypeError("Wrong type of comparing number")
         return not self < rn
 
-    def __add__(self, rn: "RationalNumber") -> "RationalNumber":
-        new_numerator: int = self._numerator * rn.get_denominator() + rn.get_numerator() * self._denominator
-        new_denominator: int = self._denominator * rn.get_denominator()
+    def __neg__(self):
+        return RationalNumber(self._numerator * (-1), self._denominator)
+
+    def __add__(self, rn: Union["RationalNumber", int]) -> "RationalNumber":
+        if isinstance(rn, int):
+            new_rn: RationalNumber = RationalNumber(rn, 1)
+        else:
+            new_rn: RationalNumber = rn
+        new_numerator: int = self._numerator * new_rn.get_denominator() + new_rn.get_numerator() * self._denominator
+        new_denominator: int = self._denominator * new_rn.get_denominator()
         return RationalNumber(new_numerator, new_denominator)
+
+    def __mul__(self, rn: Union["RationalNumber", int]) -> "RationalNumber":
+        if isinstance(rn, int):
+            new_rn: RationalNumber = RationalNumber(rn, 1)
+        else:
+            new_rn: RationalNumber = rn
+        new_numerator: int = self._numerator * new_rn.get_numerator()
+        new_denominator: int = self._denominator * new_rn.get_denominator()
+        return RationalNumber(new_numerator, new_denominator)
+
+    def __sub__(self, rn: Union["RationalNumber", int]) -> "RationalNumber":
+        if isinstance(rn, int):
+            new_rn: RationalNumber = (-RationalNumber(rn, 1))
+        else:
+            new_rn: RationalNumber = (-rn)
+        return self + new_rn
+
+    def __truediv__(self, rn: Union["RationalNumber", int]) -> "RationalNumber":
+        if isinstance(rn, int):
+            new_rn: RationalNumber = RationalNumber(1, rn)
+        else:
+            new_rn: RationalNumber = RationalNumber(rn.get_denominator(), rn.get_numerator())
+        return self * new_rn
+
+    def __pow__(self, power_number: int) -> "RationalNumber":
+        if power_number == 0:
+            return RationalNumber(1, 1)
+        if power_number < 0:
+            return RationalNumber(self._denominator ** abs(power_number), self._numerator ** abs(power_number))
+        return RationalNumber(self._numerator ** power_number, self._denominator ** power_number)
