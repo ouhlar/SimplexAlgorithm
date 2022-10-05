1301d3e952ba5343d32723a1622e076d7afc2510
duchzjety <oliverkolombo@gmail.com>
2022-10-05 23:05:00 +0200

    add more functions for matrix

--- src/matrix.py
+++ src/matrix.py
@@ -1,4 +1,4 @@
-from typing import Tuple
+from typing import Tuple, Union
 from rational_number import RationalNumber
 from vector import Vector
 
@@ -7,7 +7,7 @@ class Matrix:
     def __init__(self, *values: Vector) -> None:
         self.matrix = Vector(*values)
 
-    def __getitem__(self, item) -> "Vector":
+    def __getitem__(self, item) -> Vector:
         return self.matrix[item]
 
     def __len__(self) -> int:
@@ -17,7 +17,38 @@ class Matrix:
         return str(self.matrix)
     
     def __str__(self) -> str:
-        return str(self.matrix)
+        return '[' + '\n '.join((map(str, self.matrix))) + ']'
+    
+    def __iter__(self):
+        return self.matrix.__iter__()
+    
+    def __neg__(self) -> "Matrix":
+        return Matrix(*(-v for v in self.matrix))
 
-    def size(self) -> Tuple[int, int]:
+    def m_dimension(self) -> Tuple[int, int]:
         return len(self), len(self[0])
+
+    def __add__(self, other: "Matrix") -> "Matrix":
+        if self.m_dimension() != other.m_dimension():
+            raise ValueError("Different dimension of matrices")
+        return Matrix(*(a + b for a,b in zip(self.matrix, other.matrix)))
+    
+    def __sub__(self, other: "Matrix") -> "Matrix":
+        return self.__add__(-other)
+    
+    def __mul__(self, other: Union["Matrix", RationalNumber, int, Vector]) -> "Matrix":
+        if isinstance(other, Vector):
+            other = Matrix(*(Vector(other)))
+        if isinstance(other, Matrix):
+            if self.m_dimension()[1] != len(other):
+                raise ValueError("Wrong dimension of matrices for multiplication")
+            return Matrix(*((Vector(*(sum(a * b for a, b in zip(A_row, B_col)) for B_col in zip(*other))) for A_row in self)))
+        if isinstance(other, Union[int, RationalNumber]):
+            return Matrix(*(a * other for a in self.matrix))
+        else:
+            raise TypeError("wrong type of number for multiplication")
+    
+    def __truediv__(self, other: Union[RationalNumber, int]) -> "Matrix":
+        if not isinstance(other, Union[RationalNumber, int]):
+            raise TypeError("wrong type of number for division")
+        return Matrix(*(a / other for a in self))
--- src/vector.py
+++ src/vector.py
@@ -1,4 +1,4 @@
-from typing import Tuple, Union
+from typing import Union
 
 from rational_number import RationalNumber
 
@@ -13,34 +13,33 @@ class Vector:
     # Dot products
     def __mul__(self, other: Union[int, RationalNumber, "Vector"]) -> Union[RationalNumber, "Vector"]:
         if isinstance(other, Union[int, RationalNumber]):
-            return Vector(*(i * other for i in self))
+            return Vector(*(i * other for i in self.vector))
         if isinstance(other, Vector):
-            return sum(a * b for a, b in zip(self, other))
+            return sum(a * b for a, b in zip(self.vector, other.vector))
         else:
             raise TypeError("Wrong type of values for multiplication")
     
-    def __rmul__(self, other: Union[int, RationalNumber]) -> "Vector":
+    def __rmul__(self, other: Union[int, RationalNumber, "Vector"]) -> "Vector":
         return self.__mul__(other)
     
     def __add__(self, other: "Vector") -> "Vector":
         if isinstance(other, Vector):
             if len(self) == len(other):
-                return Vector(*(a + b for a, b in zip(self, other)))
+                return Vector(*(a + b for a, b in zip(self.vector, other.vector)))
         else:
             raise TypeError("Wrong type of values for addition")
     
+    def __neg__(self) -> "Vector":
+        return Vector(*(-rn for rn in self.vector))
+
     def __sub__(self, other: "Vector") -> "Vector":
-        if isinstance(other, Vector):
-            if len(self) == len(other):
-                return Vector(*(a - b for a, b in zip(self, other)))
-        else:
-            raise TypeError("Wrong type of values for substraction")
+        return self.__add__(-other)
     
     def __truediv__(self, other: Union[int, RationalNumber]) -> "Vector":
-        if other == 0:
-            raise ZeroDivisionError("Division by 0")
         if isinstance(other, Union[int, RationalNumber]):
-            return Vector(*(i / other for i in self))
+            if isinstance(other, int):
+                other: RationalNumber = RationalNumber(other,1)
+            return Vector(*(i / other for i in self.vector))
         else:
             raise TypeError("Wrong type of values for division")
 
@@ -49,9 +48,12 @@ class Vector:
 
     def __getitem__(self, item) -> Union["Vector", RationalNumber]:
         return self.vector[item]
+    
+    def __iter__(self):
+        return self.vector.__iter__()
 
     def __repr__(self) -> str:
         return str(self.vector)
     
     def __str__(self) -> str:
-        return str(self.vector)
+        return '[' + '  '.join((map(str, self.vector))) + ']'
